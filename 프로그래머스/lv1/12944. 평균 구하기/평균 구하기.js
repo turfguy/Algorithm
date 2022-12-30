fe268c843179e5bad207c68f09a25165aced1e74
김승민 <103026404+turfguy@users.noreply.github.com>
2022-12-30 21:26:28 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
@@ -0,0 +1,10 @@
+function solution(arr) {
+    var answer = 0;
+    for (var i=0 ; i<arr.length; i++)
+    {
+        answer+= arr[i]        
+    }
+    answer/=arr.length
+    return answer;
+    
+}

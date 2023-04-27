0b02f8028fdbdabbcfe2c747b150912e088b0ae8
SeungminKim <103026404+turfguy@users.noreply.github.com>
2023-04-27 16:33:41 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.py
@@ -1,7 +1,2 @@
 def solution(my_string):
-    my_list = list(my_string)
-    answer_list = []
-    for i in range(len(my_list)-1,-1,-1):
-        answer_list.append(my_list[i])
-    
-    return ''.join(answer_list)
+    return my_string[::-1]

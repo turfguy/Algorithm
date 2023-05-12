25555715da8b81dc3102286daa63953842e8c581
SeungminKim <103026404+turfguy@users.noreply.github.com>
2023-05-12 12:23:25 +0900

    [level 2] Title: 타겟 넘버, Time: 2.93 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,7 +1,7 @@
 def solution(numbers, target):
     answer = 0
-    def dfs(index, result):
-        if index == len(numbers):
+    def dfs(index,result):
+        if  index == len(numbers):
             if result == target:
                 nonlocal answer
                 answer+=1

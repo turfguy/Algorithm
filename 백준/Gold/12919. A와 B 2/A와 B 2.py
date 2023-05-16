931713222a94e63d3cc43a6b06eb6afa1c0d78e7
SeungminKim <103026404+turfguy@users.noreply.github.com>
2023-05-16 17:20:33 +0900

    [Gold V] Title: A와 B 2, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/12919. A와 B 2/A와 B 2.py
@@ -0,0 +1,15 @@
+import sys
+S=list(input())
+T=list(input())
+def dfs(t):
+    if t==S:
+        print(1)
+        sys.exit()
+    if len(t)==0:
+        return 0
+    if t[-1]=='A':
+        dfs(t[:-1])
+    if t[0]=='B':
+        dfs(t[1:][::-1])
+dfs(T)
+print(0)

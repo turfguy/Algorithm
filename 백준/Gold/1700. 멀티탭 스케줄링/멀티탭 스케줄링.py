1db4610d740d7a6c4046a6dd4294a0a29e54de58
SeungminKim <103026404+turfguy@users.noreply.github.com>
2023-05-17 12:47:27 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+N, K = map(int, input().split())
+use = list(map(int, input().split()))
+
+plugs = []
+result = 0
+for i in range(K):
+    if use[i] in plugs:
+        continue
+    if len(plugs) != N:
+        plugs.append(use[i])
+        continue
+    far_one = 0
+    temp = 0
+    for plug in plugs:
+        if plug not in use[i:]:
+            temp = plug
+            break
+        elif use[i:].index(plug) > far_one:
+            far_one = use[i:].index(plug)
+            temp = plug
+    plugs[plugs.index(temp)] = use[i]
+    result += 1
+
+print(result)
